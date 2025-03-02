4d5d7ae63b38fa8d97203c9ff62dfaa31fd6be29
Buggie404 <judyhoang1807@gmail.com>
2025-03-02 23:46:24 +0700

    Completed admin model

--- LibraryManagementApp/Model/admin_model.py
+++ LibraryManagementApp/Model/admin_model.py
@@ -1,25 +1,35 @@
-# models/admin.py - Quản lý Admin
-from user_model import User
-class Admin(User):
-    def __init__(self, user_id, name, username, email, password, date_of_birth, role="admin"):
-        super().__init__(user_id, name, username, email, password, date_of_birth, role)
-        
+from Model.user_model import User
+from Model.book_model import Book
+from Database.db_lma import Database
 
-    def add_user(self, user_list, new_user):
-        pass
+class Admin(User):
+    def __init__(self, user_id = None, name = None, username = None, email = None, password = None, date_of_birth = None):
+        super().__init__(user_id, name, username, email, password, date_of_birth, role="admin")
+
+    def add_user(new_user):
+        new_user.save_user()
     
-    def reset_password(self, user, new_password):
-        pass
-      
-    def delete_user(self, user_list, user):
-        pass
+    def reset_password(self, user_id):
+        default_password = "123456789"
+        self.db.cursor.execute("UPDATE users SET password = ? WHERE user_id = ?", (default_password, user_id))
+        self.db.conn.commit()
+
+    def delete_user(self, user_id):
+        self.db.cursor.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
+        self.db.conn.commit()
     
-    def add_book(self, book_list, book):
-        pass
+    def add_book(self, title, author, category, published_year, quantity):
+        book = Book(title, author, category, published_year, quantity)
+        book.save_book()
     
-    def edit_book(self, book, new_data):
-        pass
+    def edit_book(self, book_id, new_data):
+        book = Book.get_book_by_id(book_id)
+        if book:
+            book.update_book(new_data)
+            return "Updated Successfully"
+        return None
        
-    def delete_book(self, book_list, book):
-        pass
+    def delete_book(self, book_id):
+        self.db.cursor.execute("DELETE FROM books WHERE book_id = ?", (book_id,))
+        self.db.conn.commit()
        
