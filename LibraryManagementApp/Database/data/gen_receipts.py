5c8f0abdd2abf98bd94e5c75f77ef0636d61db18
Thiel <nnbn195533@gmail.com>
2025-03-21 11:45:31 +0700

    change book_id in tbl Receipt into Text type

--- LibraryManagementApp/Database/data/gen_receipts.py
+++ LibraryManagementApp/Database/data/gen_receipts.py
@@ -4,11 +4,11 @@ conn = sqlite3.connect('LibraryManagementApp/Database/library.db')
 c = conn.cursor()
 
 receipt_list = [
-    (211, 127, 1900850303, '2025-01-18', '2025-01-22', 'Returned'),
-    (212, 114, 156047624, '2025-02-02', '2025-02-05', 'Returned'),
-    (213, 136, 966986105, '2025-02-02', '', 'Overdue'),
-    (214, 164, 312953453, '2025-02-20', '', 'Borrowed'),
-    (215, 111, 316973742, '2025-02-25', '', 'Borrowed')
+    (211, 127, '0345417623', '2025-01-18', '2025-01-22', 'Returned', '1'),
+    (212, 114, '0060168013', '2025-02-02', '2025-02-05', 'Returned', '1'),
+    (213, 136, '0345465083', '2025-02-02', '', 'Overdue', '1'),
+    (214, 164, '0743403843', '2025-02-20', '', 'Borrowed', '1'),
+    (215, 111, '0553584383', '2025-02-25', '', 'Borrowed', '1')
 ]
-c.executemany("insert into Receipts values(?, ?, ?, ?, ?, ?)", receipt_list)
+c.executemany("insert into Receipts values(?, ?, ?, ?, ?, ?, ?)", receipt_list)
 conn.commit()
